85a2e7075fa2c251885dc611898312f3f3056172
Harry101010 <harryvnsg@gmail.com>
2026-04-03 22:12:26 +0700

    test?

--- proj_NN_group2/src/main/java/aptech/proj_NN_group2/PrimaryController.java
+++ proj_NN_group2/src/main/java/aptech/proj_NN_group2/PrimaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
+	// abcj
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
