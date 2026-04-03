4c5fd2821d816aff0df7f25982d521b97ea1ffa4
Harry Tran <144422693+Harry101010@users.noreply.github.com>
2026-04-03 22:13:46 +0700

    Update SecondaryController.java

--- proj_NN_group2/src/main/java/aptech/proj_NN_group2/SecondaryController.java
+++ proj_NN_group2/src/main/java/aptech/proj_NN_group2/SecondaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
-
+    // ldsjf;saldjfa;sldfkjas;d
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
