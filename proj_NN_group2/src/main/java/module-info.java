2365f0b4098f3281c31012196f601edb28e7d80d
Harry101010 <harryvnsg@gmail.com>
2026-04-18 00:29:30 +0700

    remake pppp

--- proj_NN_group2/src/main/java/module-info.java
+++ proj_NN_group2/src/main/java/module-info.java
@@ -13,6 +13,7 @@ module aptech.proj_NN_group2 {
     opens aptech.proj_NN_group2.controller.auth to javafx.fxml;
     opens aptech.proj_NN_group2.controller.production to javafx.fxml;
     opens aptech.proj_NN_group2.controller.sales to javafx.fxml;
+    opens aptech.proj_NN_group2.controller.warehouse to javafx.fxml;
 
     opens aptech.proj_NN_group2.model.entity to javafx.base;
 
@@ -21,6 +22,7 @@ module aptech.proj_NN_group2 {
     exports aptech.proj_NN_group2.controller.auth;
     exports aptech.proj_NN_group2.controller.production;
     exports aptech.proj_NN_group2.controller.sales;
+    exports aptech.proj_NN_group2.controller.warehouse;
 
     exports aptech.proj_NN_group2.model.entity;
     exports aptech.proj_NN_group2.model.business.repository;
