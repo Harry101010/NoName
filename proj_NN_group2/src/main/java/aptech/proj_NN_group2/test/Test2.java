dec3a393b1ad91fdbac7c2edaed477dc68a0486f
HoangVu-KT <nghvukt3@gmail.com>
2026-04-22 08:07:02 +0700

    pull code

---
+++ proj_NN_group2/src/main/java/aptech/proj_NN_group2/test/Test2.java
@@ -0,0 +1,17 @@
+package aptech.proj_NN_group2.test;
+
+import aptech.proj_NN_group2.model.business.repository.UserRepository;
+import aptech.proj_NN_group2.model.entity.User;
+
+public class Test2 {
+	/* Nghia */
+	public static void main(String[] args) {
+		UserRepository repo = new UserRepository();
+		System.out.println(repo.findById(10));
+		System.out.println("___________________________");
+		for (User u : repo.findAll()) {
+			System.out.println(u);
+		}
+	}
+}
+
